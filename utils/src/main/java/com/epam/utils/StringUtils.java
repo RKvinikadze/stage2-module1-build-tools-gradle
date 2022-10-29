0b434ef3387d3028a63038ff610243edb766e001
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-10-29 17:09:39 +0400

    null pointer exception

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return !str.startsWith("0") && NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
+        return str != null && !str.startsWith("0") && NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
